32ccd50dda0e0d6dad2358b005964e1e678b96ad
mrakitin <mrakitin@users.noreply.github.com>
2024-06-05 15:19:26 +0000

    deploy: 63750adc6b126b83fd00809ecee50df21825df11

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.8.3.dev503+gbc8222be',
+    VERSION: '1.8.3.dev537+g63750adc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
